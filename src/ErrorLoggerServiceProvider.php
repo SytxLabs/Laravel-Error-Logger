fbb48914d2bc50bc32714c4f26cfdd2af6d7601c
Shaun Lüdeke <77498048+shaunluedeke@users.noreply.github.com>
2024-06-05 19:32:59 +0200

    Update

--- src/ErrorLoggerServiceProvider.php
+++ src/ErrorLoggerServiceProvider.php
@@ -12,16 +12,14 @@ class ErrorLoggerServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../config/error-logger.php' => config_path('error-logger.php'),
-            ], 'config');
+            ], 'error-logger-config');
 
             AboutCommand::add('SytxLabs Error Log Package', static fn () => ['Version' => '1.0.0', 'Author' => 'SytxLabs']);
         }
     }
 
-    #[\Override]
     public function register(): void
     {
-        parent::register();
         $this->mergeConfigFrom(__DIR__ . '/../config/error-logger.php', 'error-logger');
         $this->mergeConfigFrom(__DIR__ . '/../config/channels.php', 'logging.channels');
     }
